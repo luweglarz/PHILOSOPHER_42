a47d2781ff2e5bd8cd83c7c1dac75c83aabe3062
RolandGamos <lucasweglarz9@gmail.com>
2021-09-07 22:06:14 +0200

    Comment

--- srcs/philo_actions.c
+++ srcs/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 19:11:09 by user42            #+#    #+#             */
-/*   Updated: 2021/09/07 20:54:34 by user42           ###   ########.fr       */
+/*   Updated: 2021/09/07 22:05:59 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,22 @@ t_millisecond	get_time(t_philo *philo)
 void	philo_eat(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->args->mutex);
-	if (philo->forks[0]->available == true && philo->has_eaten == false)
+	if (philo->num == 2 && (philo->forks[0]->available == 1 || philo->forks[1]->available))
+	printf("le philo %d fork 0 %u fork 1 %u\n", philo->num, philo->forks[0]->available, philo->forks[1]->available);
+	if (philo->forks[0]->available == true)
 	{
 		printf("%lu philo %d has taken a fork num %d\n", get_time(philo), philo->num, philo->forks[0]->num);
 		philo->forks[0]->available = false;
 		philo->forks[0]->owner = philo;
 	}
-	if (philo->forks[1]->available == true && philo->has_eaten == false)
+	if (philo->forks[1]->available == true)
 	{
 		printf("%lu philo %d has taken a fork num %d\n", get_time(philo), philo->num, philo->forks[1]->num);
 		philo->forks[1]->available = false;
 		philo->forks[1]->owner = philo;
 	}
 	if (philo->forks[0]->owner == philo && philo == philo->forks[1]->owner &&
-		philo->forks[1]->available == false && philo->forks[1]->available == false &&
+		philo->forks[0]->available == false && philo->forks[1]->available == false &&
 		philo->has_eaten == false)
 		{
 		printf("%lu philo %d is eating\n", get_time(philo), philo->num);
@@ -48,7 +50,9 @@ void	philo_eat(t_philo *philo)
 	if (philo->has_eaten == true)
 	{
 		while (get_time(philo) != philo->args->time_to_eat)
-		{}
+		{
+			//Philo 2 ne prend pas son autre fork car cette boucle tourne à l'infini
+		}
 		pthread_mutex_lock(&philo->args->mutex);
 		philo->forks[0]->available = true;
 		philo->forks[1]->available = true;
